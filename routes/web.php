412627ba781988dfd44b813e32a0582a036f5ca0
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-04-01 09:55:33 -0300

    inicianco a api laravel

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/home', HomeController::class)->only([
+    'index', 'show'
+]);
+
